acf37eb2c1d0a7129b5f6ebf0e3ba7a993bda6c5
Thado Zalawmpuia <zlwmpthado@gmail.com>
2022-02-07 20:51:16 +0900

    modified timezone

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,13 @@ module IgClone
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
     config.load_defaults 6.0
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
